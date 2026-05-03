5d20670e220ebe6996cc2775e967658284ca7bed
Cyril <cyril2004@example.com>
2026-05-03 10:31:08 +0100

    StatsService: unwrap backend data envelope for overall and gameweek stats

--- lib/services/stats_service.dart
+++ lib/services/stats_service.dart
@@ -8,11 +8,19 @@ class StatsService {
 
   /// Fetch overall league statistics from backend.
   Future<Map<String, dynamic>> getOverallStats() async {
-    return _apiService.get(ApiEndpoints.overallStats);
+    final res = await _apiService.get(ApiEndpoints.overallStats);
+    if (res['data'] is Map<String, dynamic>) {
+      return res['data'] as Map<String, dynamic>;
+    }
+    return <String, dynamic>{};
   }
 
   /// Fetch per-gameweek statistics from backend.
   Future<Map<String, dynamic>> getGameweekStats() async {
-    return _apiService.get(ApiEndpoints.gameweekStats);
+    final res = await _apiService.get(ApiEndpoints.gameweekStats);
+    if (res['data'] is Map<String, dynamic>) {
+      return res['data'] as Map<String, dynamic>;
+    }
+    return <String, dynamic>{};
   }
 }
